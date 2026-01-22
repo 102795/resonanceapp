31f10c6e3db2b96edf5021519da0f6ecf4efe975
Omar Ismaiel <102795@glr.nl>
2026-01-22 08:25:25 +0000

    homepage

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+    document.querySelectorAll('.album-card').forEach(card => {
+        card.addEventListener('click', () => {
+         
+            console.log('Album geklikt');
+        });
+    });
